66d779c1129968304c6112e22629775582281273
danafhoppe <danafhoppe@gmail.com>
2020-09-28 13:38:04 -0500

    Modularized hills to enable new hill founding.

--- src/Hill.js
+++ src/Hill.js
@@ -1,15 +1,43 @@
 class Hill{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-    
-    this.food = 0;
-  }
-  
-  show(){
-    stroke(0);
-    strokeWeight(1);
-    fill(this.food*10,this.food*5,this.food*2);
-    ellipse(this.x,this.y,this.food/2);
-  }
+	constructor(x,y){
+		this.x = x;
+		this.y = y;
+		
+		this.food = 0;
+	}
+	
+	show(){
+		noStroke();
+		strokeWeight(1);
+		fill(this.food,this.food/5,this.food/5);
+		ellipse(this.x,this.y,this.food/50);
+	}
+	
+	visit(ant){
+		let ant1 = ant;
+		let hill = this;
+		let reproduce = false;
+		
+		if(ant1.home){
+				if(ant1.food > ant1.limit/2){
+						reproduce = true;
+				}
+				hill.food+= ant1.food;
+				ant1.food = 0;
+				ant1.limit +=1;
+				ant1.capacity = ant1.limit;
+				
+				if(hill.food > 10){
+					if(ant1.health < 30){
+						ant1.health += 20;
+						hill.food += -1;
+					}					
+				}
+				ant1.home = false;
+				ant1.travel = true;
+			}			
+			hill.show();
+			return reproduce;
+	}
+	
 }
--- src/Main.js
+++ src/Main.js
@@ -5,131 +5,119 @@ let origin;
 
 let fr = 15;
 
-let s = 10;
+let s = 2;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
-  origin = createVector(400,400);
-  
-  for(let j = 0; j < 100; j++){    
-    let ant = new Ant(origin.x,origin.y,s);
-    ant.fertility = random(1,4);
-    ants.push(ant);
-  }
-  
-  //create ant hill
-  hill = new Hill(origin.x,origin.y);
-  
-  //generate blocks
-  for(i=0; i<=width; i+=s){
-    blocks [i] = [];
-    for(j=0; j<=height; j+=s){
-      block = new Block(i,j,s);
-      let rand = random(0,10);
-      if(rand > 9){
-        block.obstacle = true;
-      }
-      else if(rand>8.9){
-        block.food = random(50,300);
-      }
-      
-      if(i == width || i == 0 || j == height || j == 0){
-        block.obstacle = true;
-      }
-      blocks [i][j] = block;
-      block.activate();
-    }
-  }
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+	origin = createVector(400,400);
+	
+	//create ant hill
+	hill = new Hill(origin.x,origin.y);
+	
+	for(let j = 0; j < 100; j++){		
+		let ant = new Ant(origin.x,origin.y,s);
+		ant.fertility = random(1,4);
+		ant.hill = hill;
+		ants.push(ant);
+	}
+	
+	origin.x = 800;
+	origin.y = 400;
+	
+	//create ant hill
+	hill2 = new Hill(origin.x,origin.y);
+	
+	for(let j = 0; j < 100; j++){		
+		let ant = new Ant(origin.x,origin.y,s);
+		ant.fertility = random(1,4);
+		ant.hill = hill2;
+		ants.push(ant);
+	}
+	
+	
+	
+	//generate blocks
+	for(i=0; i<=width; i+=s){
+		blocks [i] = [];
+		for(j=0; j<=height; j+=s){
+			block = new Block(i,j,s);
+			let rand = random(0,10);
+			if(rand > 9.5){
+				block.obstacle = true;
+			}
+			else if(rand>9.2){
+				block.food = random(50,300);
+			}
+			
+			if(i == width || i == 0 || j == height || j == 0){
+				block.obstacle = true;
+			}
+			blocks [i][j] = block;
+			block.activate();
+		}
+	}
 }
 
 function draw() {
-  //set frame rate
-  frameRate(fr);
-  //iterate through ants
-  for(let a = 0; a < ants.length; a ++){
-    //grab ant
-    let ant1 = ants[a];
-    
-    //simulate ant if living
-    if(ant1.health > 0){
-      //generate all possible moves
-      let moves = generate_moves(ant1);
-      //current block which ant occupies
-      let current = blocks[ant1.position.x][ant1.position.y];
-      //determine ant movement
-      ant1.decide(moves, current);
-      
-      //check to see if ant is at the hill
-      if(ant1.home){
-        if(hill.food > 0){
-          if(ant1.food > (ant1.limit/2)){
-            for(i = 0; i < ant1.fertility; i++){
-              ant2 = new Ant(origin.x,origin.y,s);
-  
-              //inherit traits from parent (plus genetic mutation);
-              ant2.fertility = ant1.fertility + random(-0.1,1);
-              ant2.tolerance = ant1.tolerance + random(-1,1);
-              ant2.starvation = ant1.starvation + random(-1,1);
-              ant2.potency_modifier = ant1.potency_modifier + random(-0.01,0.01);
-              ant2.color = ant1.color;
-              ants.push(ant2);
-            }
-          }
-        }
-        //Deposit food at hill
-        hill.food+= ant1.food;
-        ant1.food = 0;
-        
-        //Increase ant carrying capacity
-        ant1.limit +=1;
-        ant1.capacity = ant1.limit;
-        
-        //Eat hill food if desperate
-        if(hill.food > 10){
-          if(ant1.health < ant1.starvation){
-            ant1.health += 30;
-            hill.food += -1;
-          }          
-        }
-        //ant returns to searching
-        ant1.home = false;
-      }      
-      hill.show();
-    }
-    //remove dead ants from simulation
-    else{
-        ants.splice(a, 1);
-        blocks[ant1.position.x][ant1.position.y].ants += -1;
-        fill(255,0,0);
-        ellipse(ant1.position.x,ant1.position.y,s);
-    }
-  }
+	//set frame rate
+	frameRate(fr);
+	//iterate through ants
+	for(let a = 0; a < ants.length; a ++){
+		//grab ant
+		let ant1 = ants[a];
+		
+		//simulate ant if living
+		if(ant1.health > 0){
+			//generate all possible moves
+			let moves = generate_moves(ant1);
+			//current block which ant occupies
+			let current = blocks[ant1.position.x][ant1.position.y];
+			//determine ant movement
+			ant1.decide(moves, current);
+			
+			//check to see if ant is at the hill
+			success = ant1.hill.visit(ant1);
+			if(success){
+				for(let i = 0; i < ant1.fertility; i++){
+					new_ant = ant1.reproduce();	
+					ants.push(new_ant);
+				}
+			}
+		}
+		//remove dead ants from simulation
+		else{
+				ants.splice(a, 1);
+				blocks[ant1.position.x][ant1.position.y].ants += -1;
+				fill(255,0,0);
+				ellipse(ant1.position.x,ant1.position.y,s);
+		}
+	}
 }
 
 //generate all possible blocks an ant could choose from
-function generate_moves(ant1) {  
-      let moves = [];
-      moves.push(blocks[ant1.position.x + s][ant1.position.y]);
-      moves.push(blocks[ant1.position.x - s][ant1.position.y]);
-      moves.push(blocks[ant1.position.x][ant1.position.y + s]);
-      moves.push(blocks[ant1.position.x][ant1.position.y - s]);
-      moves.push(blocks[ant1.position.x + s][ant1.position.y + s]);
-      moves.push(blocks[ant1.position.x - s][ant1.position.y + s]);
-      moves.push(blocks[ant1.position.x - s][ant1.position.y - s]);
-      moves.push(blocks[ant1.position.x + s][ant1.position.y - s]);
-    
-      return moves;
-  }
+function generate_moves(ant1) {	
+			let moves = [];
+			moves.push(blocks[ant1.position.x + s][ant1.position.y]);
+			moves.push(blocks[ant1.position.x - s][ant1.position.y]);
+			moves.push(blocks[ant1.position.x][ant1.position.y + s]);
+			moves.push(blocks[ant1.position.x][ant1.position.y - s]);
+			moves.push(blocks[ant1.position.x + s][ant1.position.y + s]);
+			moves.push(blocks[ant1.position.x - s][ant1.position.y + s]);
+			moves.push(blocks[ant1.position.x - s][ant1.position.y - s]);
+			moves.push(blocks[ant1.position.x + s][ant1.position.y - s]);
+		
+			return moves;
+	}
 
 //control frame rate
 function keyPressed() {
   if (keyCode === UP_ARROW) {
     fr++;
   } else if (keyCode === DOWN_ARROW) {
-    if(fr>1){
-      fr--; 
-    }    
+		if(fr>1){
+			fr--; 
+		}    
   }
   return false; // prevent default
 }
